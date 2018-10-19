87def9bc7b317c14dbd3e0d5ff1ec9df0a0ebe38
anmrdz <anmrdz@gmail.com>
2018-10-19 18:44:50 -0500

    config cors

--- proyecto/backend/config/application.rb
+++ proyecto/backend/config/application.rb
@@ -11,6 +11,14 @@ module Backend
     config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '/api/*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
